2d2d15c6ec27028e52fe9128feef6dd89b338bf6
cherukurus <cherukurus@msoe.edu>
2022-04-27 16:40:05 -0500

    removed usless comments in wumpus

--- src/wumpus.cpp
+++ src/wumpus.cpp
@@ -20,18 +20,6 @@ Wumpus::Wumpus(Map *m, int x, int y)
     alive = true;
 }
 
-/*void Wumpus::killPlayer() {
-    char player = 'p';
-    //gets the players location
-    player = map->cells[xLocation][yLocation]->display(); //Confused what is happening here
-    //if the player is at wumpus location and the wumpus is not dead, it kills the player
-    if(player == map->cells[xLocation][yLocation]->occupied()) {
-        if(!Wumpus.isDead()) {
-            cout << "Oh no, the wumpus killed you!";
-        }
-    }
-}
-*/
 bool Wumpus::isDead() {
     if(!map->cells[xLocation][yLocation]->display() == '!') { //haswumpus() doesn't return a string
         cout << "Nice job, you beat the game!";
